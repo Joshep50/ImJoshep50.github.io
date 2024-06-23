a028c828a736d523fd3f3275408e3bd9d1aed8a1
Joshep50 <166344726+Joshep50@users.noreply.github.com>
2024-06-23 01:31:11 -0600

    Add files via upload

--- dist/script.js
+++ dist/script.js
@@ -42,3 +42,50 @@ function openProject( event ) {
 function closeWindow( event ) {
   $(event.target).parents(".full-screen").fadeOut();
 }
+
+
+
+
+//--------------------------------------------
+
+
+
+
+$(document).ready(function() {
+  $('.carousel-inner .item').each(function() {
+    var $this = $(this);
+    var imgSrc = $this.find('img').attr('src');
+    var overlay = $('<div class="thumbnail-overlay"><img src="https://via.placeholder.com/50" alt="Icon"></div>');
+    $this.append(overlay);
+  });
+
+  $('.thumbnail-overlay').click(function() {
+    var src = $(this).siblings('img').attr('src');
+    $('.full-screen img').attr('src', src);
+    $('.full-screen').fadeIn();
+  });
+
+  $('.close-window').click(function() {
+    $('.full-screen').fadeOut();
+  });
+});
+
+
+$(document).ready(function() {
+  // When an image in the carousel is clicked
+  $('.carousel-inner img').on('click', function() {
+    var src = $(this).attr('src');
+    $('#fullscreenImage').attr('src', src);
+    $('#fullscreenImageModal').modal('show');
+  });
+
+  // Add zoom-in icon on hover
+  $('.carousel-inner img').hover(
+    function() {
+      $(this).css('cursor', 'zoom-in');
+    },
+    function() {
+      $(this).css('cursor', 'default');
+    }
+  );
+});
